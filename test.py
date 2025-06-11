dd970827feec6b86daf803e9a8e6eb7cf4ab629c
jesus.emiliano123@hotmail.com <jesus.emiliano123@hotmail.com>
2025-06-11 12:39:54 -0600

    Test functionalities

--- test.py
+++ test.py
@@ -1,34 +1,78 @@
 import curses
 
-KEY_EDITION = {
-    "KEY_BACKSPACE" : "BACKSPACE",
-    "\n" : "SPACEBAR",
-}
+#stdscr.clrtoeol() se utiliza para renderizar solo la linea que esta siendo editada
+#stdscr.clrtobot() se utiliza para renderizar multiples lineas
+#stdscr.clear() se utiliza para renderizar toda la pantalla. Me puede ser util para buscar archivos o querer salir del sistema.
 
-KEY_NAVEGATION = {
-    "KEY_UP" : "FLECHA ARRIBA",
-    "KEY_DOWN" : "FLECHA ABAJO",
-    "KEY_LEFT" : "FLECHA IZQUIERDA",
-    "KEY_RIGHT" : "FLECHA DERECHA"
-}
+#Configuracion inicial. Queda en duda de por que deberia implementar el primer punto
+#curses.curs_set(1) Cursos normal
+#stdscr.keypad(True) Habilita teclas especiales
 
+class Window():
+    def __init__(self, n_row, n_col):
+        self.n_row = n_row
+        self.n_col = n_col
 
+class Cursor():
+    def __init__(self, pos_y = 0, pos_x = 0):
+        self.pos_y = pos_y
+        self.pos_x = pos_x
 
-def main(stdscr):
-    stdscr.clear()
-    stdscr.refresh()
+    def up(self):
+        if self.pos_y > 0:
+            self.pos_y -= 1
+
+    def down(self, window_y):
+        if self.pos_y < window_y:
+            self.pos_y += 1
+
+    def left(self):
+        if self.pos_x > 0:
+            self.pos_x -= 1
+
+    def right(self, window_x):
+        if self.pos_x < len(window_x):
+            self.pos_x += 1
+
+def main(stdscr):    
+    #Variables del editor
+    text = ""
+    cursor = Cursor()
+    window = Window(curses.LINES - 1, curses.COLS - 1)
+
 
     while True:
-        key = stdscr.getkey()
-        
-        stdscr.addstr(0, 0, " " * 50)
+        key = stdscr.getch()
 
-        if key in KEY_EDITION:
-            stdscr.addstr(0,0, KEY_EDITION[key])
-        elif key in KEY_NAVEGATION:
-            stdscr.addstr(0,0, KEY_NAVEGATION[key])
-        # "Mostrar" el linezo (actualizar pantalla real)
+        if key == 27:
+            break
+        elif 32 <= key <= 126:
+            text += chr(key)
+        elif key == curses.KEY_BACKSPACE:
+            if text:
+                text = text[:-1]
+
+        elif key == 13 or key == 10:
+            cursor.down(window.n_row)
+            text = ""
+        
+        stdscr.move(cursor.pos_y,cursor.pos_x)
+        stdscr.clrtoeol()
+        stdscr.addstr(text)
         stdscr.refresh()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=="__main__":
     curses.wrapper(main)
